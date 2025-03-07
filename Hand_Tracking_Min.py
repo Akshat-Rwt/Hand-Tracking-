030150e06bc7a5cdd2564c19616480b9ff915252
Akshat Rawat <111777625+Akshat-Rwt@users.noreply.github.com>
2025-03-07 22:15:00 +0530

    Hand Tracking Project
    1.  Track the Hand more than 2 also.
    2.  Show the FPS which is always approx 30.
    3.  Show the Co-ordinate of the each landmarks(21 points on hand in PIXEL).
    4.  Show the ID (Particular Point on Hand ).

---
+++ Hand_Tracking_Min.py
@@ -0,0 +1,45 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime= 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    print(results.multi_hand_landmarks)
+
+    # This is used of the hand landmarks and connect them
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+
+            # Get the x and y Coordinate of all the fingers
+            for id , lm in enumerate(handLms.landmark):
+                #print(id,lm)  # This show the landmarks in decimal points or we can say that it is a ratio of the coordinate
+
+                h,w,c = img.shape
+                cx , cy = int(lm.x*w) , int(lm.y*h)
+                print(id,cx,cy)
+
+                # Detecting the landmarks putting the id number of the landmark
+                if id == 0:
+                    cv2.circle(img , (cx , cy) , 25 , (255,0,255) , cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms , mpHands.HAND_CONNECTIONS)
+
+    # Show the FPS and also maintain it
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img,str(int(fps)) , (10,70) , cv2.FONT_HERSHEY_PLAIN,3,(255,0,255) , 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
